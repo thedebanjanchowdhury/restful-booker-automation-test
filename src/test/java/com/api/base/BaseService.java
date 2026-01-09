070f6e7db38c6c309329e2e6c0430ac5efaca0d9
thedebanjanchowdhury <chowdhurydebanjan9@gmail.com>
2026-01-09 08:59:05 +0530

    configured bookingService initialization problem

--- src/test/java/com/api/base/BaseService.java
+++ src/test/java/com/api/base/BaseService.java
@@ -16,39 +16,46 @@ public class BaseService {
         requestSpecification = given().baseUri(BASE_URL);
     }
 
+    protected RequestSpecification getNewSpec() {
+        return given()
+                .baseUri(BASE_URL)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON);
+    }
+
     protected Response postRequest(Object payload, String endpoint) {
-        return requestSpecification.contentType(ContentType.JSON).body(payload).post(endpoint);
+        return getNewSpec().body(payload).post(endpoint);
     }
 
     protected Response getRequest (Object payload, String endpoint) {
-        return requestSpecification.contentType(ContentType.JSON).body(payload).post(endpoint);
+        return getNewSpec().body(payload).post(endpoint);
     }
 
     protected Response getRequest (String endpoint) {
-        return requestSpecification.get(endpoint);
+        return getNewSpec().get(endpoint);
     }
 
     protected Response getRequestWithParams (String endPoint, Map<String, Object> queryParams ) {
-        return requestSpecification.queryParams(queryParams).get(endPoint);
+        return getNewSpec().queryParams(queryParams).get(endPoint);
     }
 
     protected Response putRequest (Object payload, String endpoint, String token) {
-        return requestSpecification.contentType(ContentType.JSON).cookie("token",token).body(payload).put(endpoint);
+        return getNewSpec().cookie("token",token).body(payload).put(endpoint);
     }
 
     protected Response putRequestDenial (Object payload, String endpoint, String token) {
-        return requestSpecification.contentType(ContentType.TEXT).cookie("token",token).body(payload).put(endpoint);
+        return getNewSpec().cookie("token",token).body(payload).put(endpoint);
     }
 
     protected Response patchRequest (Object payload, String endpoint, String token) {
-        return requestSpecification.contentType(ContentType.JSON).cookie("token",token).body(payload).patch(endpoint);
+        return getNewSpec().cookie("token",token).body(payload).patch(endpoint);
     }
 
     protected Response deleteRequest (String endpoint, String token) {
-        return requestSpecification.cookie("token", token).delete(endpoint);
+        return getNewSpec().cookie("token", token).delete(endpoint);
     }
 
     protected Response deleteRequestUnauthorized (String endpoint) {
-        return requestSpecification.delete(endpoint);
+        return getNewSpec().delete(endpoint);
     }
 }
